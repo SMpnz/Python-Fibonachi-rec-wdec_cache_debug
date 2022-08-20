e70e50b22bd9ff9d273c32c24db5f48d0c350f7b
Sergey Shalaev <profff1@ya.ru>
2022-08-20 19:15:02 +0300

    update py file

--- num_fibonachi_recur with cache debug and count calls.py
+++ num_fibonachi_recur with cache debug and count calls.py
@@ -10,7 +10,7 @@ def count_calls(func):
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
         signature = ", ".join(args_repr + kwargs_repr)
         print(f"{wrapper_count_calls.num_calls}"
-        f"вызов функции {func.__name__!r}({signature})") 
+        f" вызов функции {func.__name__!r}({signature})") 
         value = func(*args, **kwargs)
         print(f"{func.__name__!r} возвращает {value!r}")
         return value
@@ -28,14 +28,18 @@ def cache(func):
     wrapper_cache.cache = dict()
     return wrapper_cache
 
-@cache #применение кеша дает возможность не высчитывать заново 
-#вызовы правой младшей половины формулы
-@count_calls #подсчет кол-ва вызовов функций 
-#и расчета значения - для наглядной оптимизации
-def fibonacci(num):
-    if num < 2:
-        return num
-    return fibonacci(num - 1) + fibonacci(num - 2)
+def main():
+    @cache #применение кеша дает возможность не высчитывать заново 
+    #вызовы правой младшей половины формулы
+    @count_calls #подсчет кол-ва вызовов функций 
+    #и расчета значения - для наглядной оптимизации
+    def fibonacci(num):
+        if num < 2:
+            return num
+        return fibonacci(num - 1) + fibonacci(num - 2)
 
-n=4
-print(f"Результат вызова функции поиск {n} числа Фибоначчи в последовательности: {fibonacci(n)}")
+    n=4
+    print(f"Результат вызова функции поиск {n} числа Фибоначчи в последовательности: {fibonacci(n)}")
+
+if __name__ == "__main__":
+    main()
